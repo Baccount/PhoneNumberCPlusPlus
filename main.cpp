4e2cec6f2ea0e37f9e54457f9270dedf084e9d0d
Brandon <jackiam2003@icloud.com>
2020-10-15 11:19:22 -0700

    more fixes

--- main.cpp
+++ main.cpp
@@ -127,7 +127,9 @@ int main()
                     break;
 
                 default:
-                    cout << "Invalid input.";
+                    cout << "Invalid" << endl;
+                    i = 50;
+                    break;
 
             }
 
